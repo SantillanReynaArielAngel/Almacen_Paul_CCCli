0b9d53c7578acf3191fef9ce2cecdda845b03c69
ivan <iwrodriguez@espe.edu.ec>
2017-11-07 18:18:12 -0500

    Creacion de Clase CrudCliente

---
+++ src/main/java/ec/edu/espe/proyecto/isii/proyecto_cccli/CCRUDCliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.proyecto.isii.proyecto_cccli;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ivan
+ */
+public class CCRUDCliente {
+    Conexion coneccion;
+    PreparedStatement sentencia;
+    ResultSet resul;
+    Connection cone;
+}
